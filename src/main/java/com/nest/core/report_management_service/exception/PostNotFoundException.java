53821fc18f840666b838daf5a0591bfe3cc815c9
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-02-20 13:38:29 -0800

    [NEST-BE-31] Create new exception for report

---
+++ src/main/java/com/nest/core/report_management_service/exception/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nest.core.report_management_service.exception;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/nest/core/report_management_service/exception/ReportPostFailException.java
@@ -0,0 +1,7 @@
+package com.nest.core.report_management_service.exception;
+
+public class ReportPostFailException extends RuntimeException {
+    public ReportPostFailException(String message) {
+        super(message);
+    }
+}
