d2bec92b160340f36363af565b9ba3e7a009816d
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-02-20 16:54:43 -0800

    [NEST-BE-33] Add function to find reports based on post id

--- src/main/java/com/nest/core/report_management_service/repository/ReportRepository.java
+++ src/main/java/com/nest/core/report_management_service/repository/ReportRepository.java
@@ -1,8 +1,18 @@
 package com.nest.core.report_management_service.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.nest.core.report_management_service.model.Report;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    @Query("SELECT r FROM Report r WHERE r.post.id = :postId AND r.post.type = 'ARTICLE'")
+    List<Report> findAllArticleReports(@Param("postId") Long articleId);
+
+    @Query("SELECT r FROM Report r WHERE r.post.id = :postId AND r.post.type = 'USERPOST'")
+    List<Report> findAllPostReports(@Param("postId") Long postId);
 }
