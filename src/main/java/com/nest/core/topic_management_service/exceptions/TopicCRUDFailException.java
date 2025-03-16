04d36ac97792fc408b6258c2a2b614a8af0b82ad
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-16 16:12:04 -0700

    [NEST-BE-45] Create exceptions for topic api for CRUD

---
+++ src/main/java/com/nest/core/topic_management_service/exceptions/TopicCRUDFailException.java
@@ -0,0 +1,7 @@
+package com.nest.core.topic_management_service.exceptions;
+
+public class TopicCRUDFailException extends RuntimeException {
+    public TopicCRUDFailException(String message) {
+        super(message);
+    }
+}
