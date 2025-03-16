6eebdd9df8f0f3764a705186c4d14bd676dffe3d
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-16 16:59:05 -0700

    [NEST-BE-40-41-43] Include more data in search response

--- src/main/java/com/nest/core/search_service/dto/SearchResponse.java
+++ src/main/java/com/nest/core/search_service/dto/SearchResponse.java
@@ -1,5 +1,7 @@
 package com.nest.core.search_service.dto;
 
+import java.util.List;
+
 import com.nest.core.post_management_service.model.Post;
 
 import lombok.AllArgsConstructor;
@@ -9,8 +11,27 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SearchResponse {
     private Long id;
+    private String title;
+    private String creatorName;
+    private String content;
+    private String topic;
+    private List<String> tags;
+    private int shareCount;
+    private Long likesCount;
+    private Long viewCount;
+    private byte[] postImage;
+
 
     public SearchResponse(Post post) {
         this.id = post.getId();
+        this.title = post.getTitle();
+        this.creatorName = post.getMember().getUsername();
+        this.content = post.getContent();
+        this.likesCount = post.getLikesCount();
+        this.shareCount = post.getShareCount();
+        this.viewCount = post.getViewCount();
+        this.tags = post.getPostTags().stream().map(postTag -> postTag.getTag().getName()).toList();
+        this.topic = post.getTopic().getName();
+        this.postImage = post.getPostImages().stream().findFirst().map(postImage -> postImage.getImageData()).orElse(null);
     }
 }
