726cab4075075d55497ebd1379598e2222d13dae
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-16 14:52:17 -0700

    [NEST-BE-44] Create exception for tag controller api

---
+++ src/main/java/com/nest/core/tag_management_service/exception/TagNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nest.core.tag_management_service.exception;
+
+public class TagNotFoundException extends RuntimeException {
+    public TagNotFoundException(String message) {
+        super(message);
+    }
+    
+}
