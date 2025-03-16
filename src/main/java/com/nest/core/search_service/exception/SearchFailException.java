0f52ee964778e3be7d498218f0f2832c035877cd
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-16 08:27:54 -0700

    [NEST-BE-40-41-43] Create exceptions for searching

---
+++ src/main/java/com/nest/core/search_service/exception/SearchFailException.java
@@ -0,0 +1,8 @@
+package com.nest.core.search_service.exception;
+
+public class SearchFailException extends RuntimeException {
+    public SearchFailException(String message) {
+        super(message);
+    }
+
+}
