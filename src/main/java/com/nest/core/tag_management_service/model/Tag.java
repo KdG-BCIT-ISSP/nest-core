95f8a1e015e0a3847697ee87f006b2494ad85b76
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-16 14:53:24 -0700

    [NEST-BE-44] Add builder and setter to model

--- src/main/java/com/nest/core/tag_management_service/model/Tag.java
+++ src/main/java/com/nest/core/tag_management_service/model/Tag.java
@@ -1,18 +1,21 @@
 package com.nest.core.tag_management_service.model;
 
-import com.nest.core.post_management_service.model.Post;
 import com.nest.core.post_management_service.model.PostTag;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Builder
 @Table(name="tag")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Tag {
