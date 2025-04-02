40eb3c461893a5a7ad7613ee3372ee6d9c5a820a
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-04-02 00:13:25 -0700

    [NEST-BE-50] Add exception for getting most active

---
+++ src/main/java/com/nest/core/post_management_service/exception/GetActivePermissionException.java
@@ -0,0 +1,7 @@
+package com.nest.core.post_management_service.exception;
+
+public class GetActivePermissionException extends RuntimeException {
+    public GetActivePermissionException(String message) {
+        super(message);
+    }
+}
