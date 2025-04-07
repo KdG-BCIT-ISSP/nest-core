74730ac2cebcf390924e411d134c18c71769b7fc
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-04-06 20:52:04 -0700

    [NEST-BE-Fix] Refactor to use comment object in model instead of id to allow for passing contentId in DTO

--- src/main/java/com/nest/core/report_management_service/dto/ReportCommentRequest.java
+++ src/main/java/com/nest/core/report_management_service/dto/ReportCommentRequest.java
@@ -2,6 +2,7 @@ package com.nest.core.report_management_service.dto;
 
 import java.sql.Date;
 
+import com.nest.core.comment_management_service.model.Comment;
 import com.nest.core.member_management_service.model.Member;
 import com.nest.core.report_management_service.model.Report;
 
@@ -13,12 +14,12 @@ import lombok.Getter;
 public class ReportCommentRequest {
     private String reason;
 
-    public Report toEntity(Member member, Long commentId) {
+    public Report toEntity(Member member, Comment comment) {
         return Report.builder()
             .member(member)
             .reason(reason)
             .createdAt(new Date(System.currentTimeMillis()))
-            .commentId(commentId)
+            .comment(comment)
             .build();
     }
 }
